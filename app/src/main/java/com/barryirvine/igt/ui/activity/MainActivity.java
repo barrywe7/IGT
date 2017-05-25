2cbc7b3346797f548f054f6847778cb8881fc5d3
barryirvine <barry@we7.com>
2017-05-25 10:21:08 +0100

    Put back line I commented out to check that tests were working!

--- app/src/main/java/com/barryirvine/igt/ui/activity/MainActivity.java
+++ app/src/main/java/com/barryirvine/igt/ui/activity/MainActivity.java
@@ -1,9 +1,9 @@
 package com.barryirvine.igt.ui.activity;
 
 import android.databinding.DataBindingUtil;
+import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
 import com.barryirvine.igt.R;
 import com.barryirvine.igt.databinding.ActivityMainBinding;
@@ -16,6 +16,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        //setSupportActionBar(mBinding.toolbar);
+        setSupportActionBar(mBinding.toolbar);
     }
 }
